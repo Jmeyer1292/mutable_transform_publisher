f0ee088f1387b8c0113cd5adbd66a39909c104b3
Jonathan Meyer <Jmeyer1292@gmail.com>
2018-06-03 00:13:04 -0500

    Implemented node logic for optionally loading and saving the state of the mutable publisher

--- src/mutable_transform_publisher_node.cpp
+++ src/mutable_transform_publisher_node.cpp
@@ -2,37 +2,64 @@
 #include "mutable_transform_publisher/mutable_transform_publisher.h"
 #include "mutable_transform_publisher/yaml_serialization.h"
 
+bool loadAndAddPublishers(const std::string& yaml_path,
+                          mutable_transform_publisher::MutableTransformPublisher& pub)
+{
+  std::vector<geometry_msgs::TransformStamped> tfs;
+  if (!mutable_transform_publisher::deserialize(yaml_path, tfs))
+  {
+    ROS_ERROR_STREAM("Unable to parse yaml file at: " << yaml_path);
+    return false;
+  }
+
+  for (const auto& t : tfs)
+  {
+    if (!pub.add(t, ros::Duration(1.0)))
+    {
+      ROS_ERROR_STREAM("Unable to add transform");
+      return false;
+    }
+  }
+  return true;
+}
+
+bool savePublishers(const std::string& yaml_path,
+                    const mutable_transform_publisher::MutableTransformPublisher& pub)
+{
+  const auto new_tfs = pub.getAllTransforms();
+
+  if (!mutable_transform_publisher::serialize(yaml_path, new_tfs))
+  {
+    std::cerr << "mutable_transform_publisher: Unable to serialize transforms to " << yaml_path << "\n";
+    return false;
+  }
+  return true;
+}
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "mutable_tf_publisher", ros::init_options::AnonymousName);
   ros::NodeHandle nh, pnh ("~");
 
-//  std::string yaml_path;
-//  if (!pnh.getParam("yaml_path", yaml_path))
-//  {
-//    ROS_FATAL_STREAM("mutable_transform_publisher requires the 'yaml_path' private parameter to be set");
-//    return 1;
-//  }
+  std::string yaml_path;
+  const bool yaml_specified = pnh.getParam("yaml_path", yaml_path);
+
+  const bool commit = pnh.param<bool>("commit", true);
 
   // Create the publisher
   mutable_transform_publisher::MutableTransformPublisher pub (nh);
 
-  // Parse the yaml file
-//  std::vector<geometry_msgs::TransformStamped> tfs;
-//  if (!mutable_transform_publisher::deserialize(yaml_path, tfs))
-//  {
-
-//  }
+  if (yaml_specified)
+  {
+    if (!loadAndAddPublishers(yaml_path, pub)) return 1;
+  }
 
   ros::spin();
 
-//  const auto new_tfs = pub.getAllTransforms();
-
-//  if (!mutable_transform_publisher::serialize(yaml_path, new_tfs))
-//  {
-//    std::cerr << "mutable_transform_publisher: Unable to serialize transforms to " << yaml_path << "\n";
-//    return 1;
-//  }
+  if (yaml_specified && commit)
+  {
+    if (!savePublishers(yaml_path, pub)) return 2;
+  }
 
   return 0;
 }
